6dbcbaf4f6856f23a2683c4cc2a80b72ab71b0a8
Deuri-mojica <84188101+Deuri-mojica@users.noreply.github.com>
2021-07-15 16:36:11 -0400

    actualizar

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,28 @@
+window.addEventListener("load", event =>{
+ event.preventDefault();
+  let  formularioGastos = document.getElementById("form");
+    formularioGastos.addEventListener('submit', event => {
+        event.preventDefault()
+        let listadosDeGastos = {
+        descripcion: document.getElementById("descripcion").value,
+        categoria: document.getElementById("Categorias").value,
+        fecha: document.getElementById("fecha").value,
+        monto: document.getElementById("monto").value,
+        moneda: document.getElementById("flexRadioDefault1").value
+      }
+ 
+     let  gurdarDatos  = (listadosDeGastos) => {
+         gastos = listadosDeGastos;
+      let  lista = JSON.parse(localStorage.getItem("gastos")) || [];
+        lista.push(gastos);
+        
+        localStorage.setItem("gastos",JSON.stringify(lista));
+        formularioGastos.reset();
+        
+     };
+    
+  gurdarDatos(listadosDeGastos);
+  
 
-function guardarDB(arreglo){
-  let  datos = arreglo;
- let datosLocalStore = JSON.stringify(datos);
- localStorage.setItem("datosForm",datosLocalStore);
-}
-
-
+  });
+});
