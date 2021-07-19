40958ac695f770d3d27b5398adf19ff5a14382d4
Deuri-mojica <84188101+Deuri-mojica@users.noreply.github.com>
2021-07-18 20:31:58 -0400

    actulizacion

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,8 @@
 window.addEventListener("load", event =>{
- event.preventDefault();
+  event.preventDefault();
   let  formularioGastos = document.getElementById("form");
     formularioGastos.addEventListener('submit', event => {
-        event.preventDefault()
+      event.preventDefault();
         let listadosDeGastos = {
         descripcion: document.getElementById("descripcion").value,
         categoria: document.getElementById("Categorias").value,
@@ -10,19 +10,56 @@ window.addEventListener("load", event =>{
         monto: document.getElementById("monto").value,
         moneda: document.getElementById("flexRadioDefault1").value
       }
- 
+      
      let  gurdarDatos  = (listadosDeGastos) => {
          gastos = listadosDeGastos;
       let  lista = JSON.parse(localStorage.getItem("gastos")) || [];
-        lista.push(gastos);
-        
+        lista.push(gastos);  
         localStorage.setItem("gastos",JSON.stringify(lista));
         formularioGastos.reset();
+        alertaFormulario();
         
      };
     
   gurdarDatos(listadosDeGastos);
   
+  
 
   });
-});
+  });
+
+window.addEventListener("load", (event) =>{
+  event.preventDefault();
+ let datos =JSON.parse(localStorage.getItem("gastos"));
+ 
+ renderizarGastos = document.getElementById("listado-gastos");
+ renderizarGastos.innerHTML = "";
+ if (datos ===null){
+   alertasGastos()
+ }else{
+  datos.forEach(element =>{
+  renderizarGastos.innerHTML = renderizarGastos.innerHTML+= 
+  `<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title title">Tarjeta de Gastos</h5>
+    <p class="card-text parrafo">${element.descripcion}</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <p><span>Categoria:</span> ${element.categoria}</span>
+    <p><span> Fecha :</span>${element.fecha}</p>
+  </ul>
+  <div class="card-body p-2">
+  <span>Monto:</span>
+  <span>${element.monto}</span>
+  <span>${element.moneda}</span>
+  
+  </div>
+</div>
+` 
+
+}) 
+ }
+
+   
+ });
+
