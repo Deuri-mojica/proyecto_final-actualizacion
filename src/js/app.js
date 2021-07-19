45bbdd376ab438133777e747ff97dce258fd0dcd
Angel Lopez <enmanuelm15@hotmail.com>
2021-07-19 19:50:10 -0400

    Validacion agregada en el registro de los gastos

--- src/js/app.js
+++ src/js/app.js
@@ -1,45 +1,71 @@
-window.addEventListener("load", event =>{
-  event.preventDefault();
-  let  formularioGastos = document.getElementById("form");
-    formularioGastos.addEventListener('submit', event => {
-      event.preventDefault();
-        let listadosDeGastos = {
-        descripcion: document.getElementById("descripcion").value,
-        categoria: document.getElementById("Categorias").value,
-        fecha: document.getElementById("fecha").value,
-        monto: document.getElementById("monto").value,
-        moneda: document.getElementById("flexRadioDefault1").value
-      }
-      
-     let  gurdarDatos  = (listadosDeGastos) => {
-         gastos = listadosDeGastos;
-      let  lista = JSON.parse(localStorage.getItem("gastos")) || [];
-        lista.push(gastos);  
-        localStorage.setItem("gastos",JSON.stringify(lista));
-        formularioGastos.reset();
-        alertaFormulario();
-        
-     };
-    
-  gurdarDatos(listadosDeGastos);
-  
+window.addEventListener("load", event => {
+  let formularioGastos = document.getElementById("form");
+  let gastos;
+  let listadosDeGastos
+  (function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+
+          }
+          else {
+            event.preventDefault();
+            listadosDeGastos = {
+              descripcion: document.getElementById("descripcion").value,
+              categoria: document.getElementById("Categorias").value,
+              fecha: document.getElementById("fecha").value,
+              monto: document.getElementById("monto").value,
+              moneda: document.getElementById("flexRadioDefault1").value
+            }
+
+            let gurdarDatos = (listadosDeGastos) => {
+              gastos = listadosDeGastos;
+              let lista = JSON.parse(localStorage.getItem("gastos")) || [];
+              lista.push(gastos);
+              localStorage.setItem("gastos", JSON.stringify(lista));
+              formularioGastos.reset();
+              alertaFormulario();
+
+            };
+
+            gurdarDatos(listadosDeGastos);
+
+
+          }
+
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
   
 
-  });
-  });
 
-window.addEventListener("load", (event) =>{
+
+
+
+});
+
+window.addEventListener("load", (event) => {
   event.preventDefault();
- let datos =JSON.parse(localStorage.getItem("gastos"));
- 
- renderizarGastos = document.getElementById("listado-gastos");
- renderizarGastos.innerHTML = "";
- if (datos ===null){
-   alertasGastos()
- }else{
-  datos.forEach(element =>{
-  renderizarGastos.innerHTML = renderizarGastos.innerHTML+= 
-  `<div class="card" style="width: 18rem;">
+  let datos = JSON.parse(localStorage.getItem("gastos"));
+
+  renderizarGastos = document.getElementById("listado-gastos");
+  renderizarGastos.innerHTML = "";
+  if (datos === null) {
+    alertasGastos()
+  } else {
+    datos.forEach(element => {
+      renderizarGastos.innerHTML = renderizarGastos.innerHTML +=
+        `<div class="card" style="width: 18rem;">
   <div class="card-body">
     <h5 class="card-title title">Tarjeta de Gastos</h5>
     <p class="card-text parrafo">${element.descripcion}</p>
@@ -55,11 +81,11 @@ window.addEventListener("load", (event) =>{
   
   </div>
 </div>
-` 
+`
 
-}) 
- }
+    })
+  }
 
-   
- });
+
+});
 
